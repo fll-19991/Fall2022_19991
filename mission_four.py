617182609b53a851c9a03451044598a6fb58f912
fll-19991 <alisa.planson@gmail.com>
2022-09-21 16:33:43 -0400

    Change comment

--- mission_four.py
+++ mission_four.py
@@ -7,7 +7,7 @@ from pybricks.iodevices import *
 from pybricks.tools import wait
 from pybricks.hubs import EV3Brick
 from robot_19991 import robot_19991
-##
+## Example
 def mission_four(r):
     print("Running Mission 4")
     r.ev3.screen.draw_text(30, 60, "Mission 4")
