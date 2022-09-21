4ef3492b5b53c50d273a85ece583ae5f4958169b
fll-19991 <alisa.planson@gmail.com>
2022-09-21 16:56:59 -0400

    Help

--- mission_one.py
+++ mission_one.py
@@ -7,7 +7,7 @@ from pybricks.iodevices import *
 from pybricks.tools import wait
 from pybricks.hubs import EV3Brick
 from robot_19991 import robot_19991
-#added comment, this time with extreme gusto
+#added comment, this time with extreme gusto  poorly
 def mission_one(r):
     print("Running Mission 1")
 r.robot.settings(straight_speed=600, straight_acceleration=200)
