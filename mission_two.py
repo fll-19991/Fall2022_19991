fb3d76c475ba2c63653f3392d720a248b3b0a9b3
team 19991 <alisa.planson@gmail.com>
2022-10-21 18:42:53 -0400

    Emily Ellia 10/19 edits

--- mission_two.py
+++ mission_two.py
@@ -13,12 +13,21 @@ def mission_two(r):
     r.ev3.screen.draw_text(30, 60, "Mission 2")
     wait(1000)
     r.ev3.screen.clear()
-
     r.robot.settings(straight_speed=1000, straight_acceleration=100, turn_rate=100, turn_acceleration=100)
-   
-    r.robot.straight(150)
-    r.robot.turn(-70)
-    r.robot.straight(800)
-    r.left_attachment_motor.run_angle(400,40)
-    r.left_attachment_motor.stop
+   #head out just a bit
     r.robot.straight(-200)
+    #turn to toy factory
+    r.robot.turn(65)
+    #Got to toy factory
+    r.robot.straight(-610)
+    #Dump in toy factory
+    r.left_attachment_motor.run_angle(70,100)
+    r.left_attachment_motor.stop
+    #Back Up
+    r.robot.straight(200)
+    #arm OUT OF MY WAY
+    r.left_attachment_motor.run_angle(-70,100)
+    #to the next one
+    r.robot.turn(60)
+    r.robot.straight(-395)
+    r.robot.turn(45)
