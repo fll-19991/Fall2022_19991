97ed46eefd935eec9f2f48ff51d6f0de82242075
fll-19991 <alisa.planson@gmail.com>
2022-10-21 18:56:55 -0400

    Hannah 10-21 changes

--- mission_four.py
+++ mission_four.py
@@ -15,9 +15,13 @@ def mission_four(r):
     r.ev3.screen.draw_text(30, 60, "Mission 4")
     wait(1000)
     r.ev3.screen.clear()
-    r.robot. straight(1200)
-    r.robot.turn(110)
-    r.robot.straight(-200)
-    r.left_attachment_motor.run_angle(-200,-110)
-    r.robot.straight(200)
- 
+    r.robot. straight(870)
+    r.left_attachment_motor.run_angle(-200,50)
+    r.left_attachment_motor.run_angle(-200,-50)
+    r.left_attachment_motor.run_angle(-200,50)
+    r.left_attachment_motor.run_angle(-200,-50)
+    r.left_attachment_motor.run_angle(-200,50)
+    r.left_attachment_motor.run_angle(-200,-50)
+    r.left_attachment_motor.run_angle(-200,50)
+
+  
