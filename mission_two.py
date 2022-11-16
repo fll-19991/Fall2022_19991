55d5f05f4201d53d20dfe00513d6b899dd5582cd
fll-19991 <alisa.planson@gmail.com>
2022-11-16 16:28:28 -0500

    changes 11-16

--- mission_two.py
+++ mission_two.py
@@ -13,8 +13,9 @@ def mission_two(r):
     r.ev3.screen.draw_text(30, 60, "Mission 2")
     wait(1000)
     r.ev3.screen.clear()
+
     r.robot.settings(straight_speed=2000, straight_acceleration=250, turn_rate=100, turn_acceleration=100)
-   #head out just a bit
+    #head out just a bit
     r.robot.straight(200)
     #turn to toy factory
     r.robot.turn(-60)
@@ -23,11 +24,20 @@ def mission_two(r):
     #Dump in toy factory
     r.left_attachment_motor.run_angle(-60,-100)
     r.left_attachment_motor.stop
-    #arm OUT OF MY WAY
+    #arm OUT OF MY WAYs
     r.left_attachment_motor.run_angle(-35,100)
     #Back Up
-    r.robot.straight(-220)
-    #to the next one
-    r.robot.turn(-70)
+    r.robot.straight(-400)
+    #To the next one
+    r.robot.turn(-83)
+
+    #Back Up
+    #r.robot.straight(-220)
+    #to the next HOME
+    #r.robot.turn(-73)
     #to the other home 
-    r.robot.straight(2700)
+    #r.robot.straight(2700)
+    #r.robot.drive(800,0)
+    #wait(5000)
+    #r.robot.stop()
+    #THIS IS DA CODE DAT WORKSSSSSSSSSSSSSSSSSSSS 21 SECSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
